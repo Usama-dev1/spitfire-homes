b17ccf8765a8eef7b4be45a439b4af3e2eb00983
Usama-dev1 <raja.usamah@gmail.com>
2026-05-05 01:19:32 +0500

    added cross icon in side menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const hamburger = document.getElementById("hamburger-icon");
 const sideMenu = document.getElementById("side-menu");
+const openIcon = document.getElementById("open-icon");
+const closeIcon = document.getElementById("close-icon");
 
 hamburger.addEventListener("click", () => {
   sideMenu.classList.toggle("hidden");
   sideMenu.classList.add("absolute");
+  openIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
 });
