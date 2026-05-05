c2eb1149d5f0f518f45d11e2b0b00ec5200901da
Usama-dev1 <raja.usamah@gmail.com>
2026-05-05 09:24:39 +0500

    added slider script html fixes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//mobile menu toggle
 const hamburger = document.getElementById("hamburger-icon");
 const sideMenu = document.getElementById("side-menu");
 const openIcon = document.getElementById("open-icon");
@@ -9,3 +10,21 @@ hamburger.addEventListener("click", () => {
   openIcon.classList.toggle("hidden");
   closeIcon.classList.toggle("hidden");
 });
+
+// image slider
+const images = [
+  "./images/slider_images/1.jpg",
+  "./images/slider_images/2.webp",
+  "./images/slider_images/3.webp",
+  "./images/slider_images/4.webp",
+  "./images/slider_images/5.webp",
+  "./images/slider_images/6.webp",
+];
+
+let currentIndex = 0;
+const sliderImg = document.getElementById("hero-slider");
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % images.length;
+  sliderImg.src = images[currentIndex];
+}, 3000);
